3c3d51b1b79f092c008fdf8a38760f7c0ae988a9
Larasatiindaryani <88530090+Larasatiindaryani@users.noreply.github.com>
2021-10-08 16:57:19 +0700

    TowerDefense

---
+++ TowerDefense/Assets/Scripts/Data.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour {
+	public static float coin = 300;
+	public static bool isGameOver = false;
+	public static bool isComplate = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
